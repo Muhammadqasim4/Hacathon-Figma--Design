2075d5a529525981166030638d6f7e0860a49859
Muhammad Qasim <156399289+Muhammadqasim4@users.noreply.github.com>
2024-12-09 10:04:08 -0800

    Update Subhero.tsx

--- src/app/components/Subhero.tsx
+++ src/app/components/Subhero.tsx
@@ -1,35 +1,27 @@
-import Image from "next/image"
-import Dining from "@/app/assests/Dinning.png"
-import Living from "@/app/assests/Image-living room.png" 
-import Bedroom from "@/app/assests/Bedroom.png" 
-export default function SubHero(){
+import Image from "next/image";
+import Dining from "@/app/assests/Dinning.png";
+import Living from "@/app/assests/Image-living room.png";
+import Bedroom from "@/app/assests/Bedroom.png";
 
-    return (
-        <>
-        <div className="subText">
-            <h3> Browse The Range</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="imgArea">
-            <div className="imgOne">
-
-            <Image src={Dining} width={282} alt="Picture of the author" />
-                <h6>Dining</h6>
-            </div>
-                
-
-            <div className="imgTow">
-            <Image src={Living} width={282} alt="Picture of the author" />
-            <h6>Living</h6>
-            </div>
-            
-            <div className="imgThree">
-
-            <Image src={Bedroom} width={282} alt="Picture of the author" />
-            <h6>Bedroom</h6>    
-            </div>
-           </div>
-
-        </>
-    )
+export default function SubHero() {
+  return (
+    <section className="sub-hero">
+      <div className="sub-hero__text">
+        <h3>Browse The Range</h3>
+        <p>Discover furniture collections to transform your living spaces.</p>
+      </div>
+      <div className="sub-hero__gallery">
+        {[
+          { img: Dining, label: "Dining" },
+          { img: Living, label: "Living" },
+          { img: Bedroom, label: "Bedroom" },
+        ].map((item, index) => (
+          <div className="sub-hero__item" key={index}>
+            <Image src={item.img} width={282} alt={`${item.label} area`} />
+            <h6>{item.label}</h6>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
